5fea9b299407ccf61ef168501c1b94755503f679
Colin Chen <colinc@fastmail.com>
2020-11-04 12:17:05 -0800

    setup new config for GPU machine

--- infer.sh
+++ infer.sh
@@ -1,5 +1,5 @@
-MODEL_DIRECTORY=/home/gutturale/code/data/esp_train_results/2020-10/carla_town01_epoch10_B1_A5_T20_precog_SocialConvRNN
+MODEL_DIRECTORY=/home/fireofearth/code/data/esp_train_results/2020-11/carla_town01_B10_A5_T20_precog_SocialConvRNN
 python $PRECOGROOT/precog/esp_infer.py \
     dataset=carla_town01_A5_T20_test \
     model.directory=$MODEL_DIRECTORY \
-    main.compute_metrics=false \
+    main.compute_metrics=true \
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-MODEL_DIRECTORY=/home/gutturale/code/data/esp_train_results/2020-10/carla_town01_epoch10_B1_A5_T20_precog_SocialConvRNN
+MODEL_DIRECTORY=/home/fireofearth/code/data/esp_train_results/2020-11/carla_town01_B10_A5_T20_precog_SocialConvRNN
 python $PRECOGROOT/precog/esp_test.py \
     dataset=carla_town01_A5_T20_test \
     model.directory=$MODEL_DIRECTORY \
